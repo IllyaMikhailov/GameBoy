627f0761958f5fa6466a0b2abec7d2dea47d48a0
DESKTOP-AKI32M6\Robocode 8 <robocodekh@gmail.com>
2025-05-17 11:35:10 +0300

    mainMenu

--- Game/sketch_may10a/sketch_may10a.ino
+++ Game/sketch_may10a/sketch_may10a.ino
@@ -1,81 +1,113 @@
 #include <Blocks.h>
 #include <GameBoy.h>
 GameBoy gb;
+void mainRacing();
+void mainSnake();
+int mode;
 byte ARROWS[8][8] = {
-{0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 1, 0, 0, 1, 0, 0},
-{0, 1, 1, 0, 0, 1, 1, 0},
-{1, 1, 1, 0, 0, 1, 1, 1},
-{0, 1, 1, 0, 0, 1, 1, 0},
-{0, 0, 1, 0, 0, 1, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0}
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 1, 1, 0, 0, 1, 1, 0},
+  {1, 1, 1, 0, 0, 1, 1, 1},
+  {0, 1, 1, 0, 0, 1, 1, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}
 };
 byte CAR[8][8] = {
-{0, 1, 0, 1, 0, 0, 0, 0},
-{0, 0, 1, 0, 0, 0, 0, 0},
-{0, 1, 1, 1, 0, 0, 0, 0},
-{0, 0, 1, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 1, 0, 0},
-{0, 0, 0, 0, 1, 1, 1, 0},
-{0, 0, 0, 0, 0, 1, 0, 0},
-{0, 0, 0, 0, 1, 0, 1, 0}
+  {0, 1, 0, 1, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 0},
+  {0, 1, 1, 1, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 0, 0, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0, 1, 0, 0},
+  {0, 0, 0, 0, 1, 0, 1, 0}
 };
 byte SNAKE[8][8] = {
-{0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 1, 1, 1, 1, 0, 0},
-{0, 1, 1, 0, 0, 1, 1, 0},
-{0, 1, 1, 0, 0, 0, 0, 0},
-{0, 0, 1, 1, 1, 1, 0, 0},
-{0, 0, 0, 0, 0, 1, 1, 0},
-{0, 1, 1, 0, 0, 1, 1, 0},
-{0, 0, 1, 1, 1, 1, 0, 0}
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 1, 1, 1, 1, 0, 0},
+  {0, 1, 1, 0, 0, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0, 0},
+  {0, 0, 1, 1, 1, 1, 0, 0},
+  {0, 0, 0, 0, 0, 1, 1, 0},
+  {0, 1, 1, 0, 0, 1, 1, 0},
+  {0, 0, 1, 1, 1, 1, 0, 0}
 };
+int modeCount;
 void setup() {
-gb.begin(13); 
-
+  gb.begin(13);
 }
-
 void loop() {
-mainMenu(); 
-Car();
+  mainMenu();
+  if (gb.getKey() == 2 && modeSelector() == 0){
+    gb.clearDisplay();
+    mode = 1;
+  }
+  else if (gb.getKey() == 2 && modeSelector() == 1){
+    gb.clearDisplay();
+    mode = 2;
+  }
+   else if (gb.getKey() == 1){
+    gb.clearDisplay();
+    mode = 0;
+   }
+   switchMode(mode);
 }
-void mainMenu(){
-  for (int i = 0; i < 8; i++){
-    for (int j = 0; j < 8; j++){
-      gb.wipePoint(i,j);
-      gb.setLed(i,j,ARROWS[j][i]);
+void mainMenu() {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.wipePoint(i, j);
+      gb.setLed(i, j, ARROWS[j][i]);
+    }
+  }
+  if (modeSelector() == 1) {
+    for (int i = 0; i < 8; i++) {
+      for (int j = 8; j < 16; j++) {
+        gb.wipePoint(i, j);
+        gb.setLed(i, j, CAR[j - 8][i]);
+      }
+    }
+  }
+  if (modeSelector() == 1) {
+    for (int i = 0; i < 8; i++) {
+      for (int j = 8; j < 16; j++) {
+        gb.wipePoint(i, j);
+        gb.setLed(i, j, SNAKE[j - 8][i]);
+      }
     }
   }
 }
-void Car(){
-   for (int i = 0; i < 8; i++){
-    for (int j = 8; j < 16; j++){
-      gb.wipePoint(i,j);
-      gb.setLed(i,j,CAR[j-8][i]);
-    }
-  }
-}
-void Snake(){
-  for (int i = 0; i < 8; i++){
-    for (int j = 8; j < 16; j++){
-      gb.wipePoint(i,j);
-      gb.setLed(i,j,CAR[j-8][i]);
-}
-int modeSelector(){
-  if (gb.getKey()==4){
+int modeSelector() {
+  if (gb.getKey() == 4) {
     modeCount++;
     delay(250);
-    if (modeCount > 1){
+    if (modeCount > 1) {
       modeCount = 0;
     }
   }
-  else if (gb.getKey() == 5){
+  else if (gb.getKey() == 5) {
     modeCount--;
     delay(250);
-    if (modeCount < 0){
+    if (modeCount < 0) {
       modeCount = 1;
     }
   }
   return modeCount;
+}
+void switchMode(int mode){
+  switch(mode){
+    case 0:mainMenu();
+    break;
+    case 1:mainRacing();
+    break;
+    case 2:mainSnake();
+    break;
+  }
+}
+void mainRacing(){
+  //code
+}
+void mainSnake(){
+  //code
 }
